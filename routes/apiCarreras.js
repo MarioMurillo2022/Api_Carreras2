f480c1931a41824530cdc14f272c034588258f25
MarioMurillo2022 <Mariomun17@gmail.com>
2023-10-18 23:50:31 -0600

    Intento de Post no exitoso

--- routes/apiCarreras.js
+++ routes/apiCarreras.js
@@ -10,4 +10,28 @@ apiCarreras.get('', async (req, res) => {
 
 });
 
+apiCarreras.post('', async (req, res) => {
+
+
+    const params = [
+        req.body.nombre,
+        req.body.equipo,
+        req.body.nombre,
+        req.body.mejor_puesto,
+        req.body.victorias,
+        req.body.poles,
+        req.body.tiempo,
+        req.body.fecha
+    ];
+
+    let sql = `insert into
+    tbl_api_carreras(nombre, equipo, mejor_puesto, victorias, poles, tiempo, fecha) 
+    values ($1,$2,$3,$4,$5,$6,$7)
+    returning * `;
+    const result = await db.query(sql, params);
+    res.json(result);
+
+});
+
+
 module.exports = apiCarreras;
